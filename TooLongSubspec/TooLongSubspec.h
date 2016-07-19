096a0afcb0cc56ab87e0e0068c802fd96eb5fe58
Yuki Nagai <ynagai@r.recruit.co.jp>
2016-07-19 18:15:17 +0900

    v1.0.0

---
+++ TooLongSubspec/TooLongSubspec.h
@@ -0,0 +1,19 @@
+//
+//  TooLongSubspec.h
+//  TooLongSubspec
+//
+//  Created by Yuki Nagai on 7/19/16.
+//  Copyright © 2016 uny, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TooLongSubspec.
+FOUNDATION_EXPORT double TooLongSubspecVersionNumber;
+
+//! Project version string for TooLongSubspec.
+FOUNDATION_EXPORT const unsigned char TooLongSubspecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TooLongSubspec/PublicHeader.h>
+
+
